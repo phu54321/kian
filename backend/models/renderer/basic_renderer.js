8a78234d2b8caa6c93a3e9cde77334f0a79e207d
phu54321 <phu54321@naver.com>
2018-07-27 04:31:54 +0900

    basic renderCard

--- backend/models/renderer/basic_renderer.js
+++ backend/models/renderer/basic_renderer.js
@@ -1,5 +1,11 @@
 module.exports = {
     cardCount(_card) {
         return 1;
+    },
+    renderCard(card, _style, _cardIndex) {
+        return {
+            front: card.fields.Front,
+            back: card.fields.Back,
+        };
     }
 };
--- tests/test_basic_renderer.js
+++ tests/test_basic_renderer.js
@@ -2,11 +2,34 @@ const BasicRenderer = require('../backend/models/renderer/basic_renderer');
 const assert = require('assert').strict;
 
 const card = {
-    fields: 'Test1\x1fTest2'
+    fields: {
+        Front: 'Test1',
+        Back: 'Test2',
+    }
 };
 
+const dumbStyle = {
+    name: 'Basic 1',
+    template: [
+        {
+            front: '{{Front}}',
+            back: '{{Back}}'
+        }
+    ]
+};
+
+
 describe('basic_renderer', function() {
-    it('should only have one card', function() {
+    it('should only create one card', function() {
         assert.equal(BasicRenderer.cardCount(card), 1);
     });
+
+    it('should render front side properly', function() {
+        assert.deepEqual(
+            BasicRenderer.renderCard(card, dumbStyle, 0),
+            {
+                front: 'Test1',
+                back: 'Test2',
+            }
+        );    });
 });
