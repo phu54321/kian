e5a5bbff751461843f552f7d6b180ef1bd058639
phu54321 <phu54321@naver.com>
2018-07-27 05:37:58 +0900

    Basic cloze rendering

---
+++ backend/models/renderer/cloze_renderer.js
@@ -0,0 +1,10 @@
+module.exports = {
+    renderCards (_card, _style) {
+        return {
+            'Cloze 1': {
+                front: '__***[..]***__',
+                back: '__**n**__'
+            }
+        };
+    }
+};
---
+++ tests/test_cloze_renderer.js
@@ -0,0 +1,29 @@
+const ClozeRenderer = require('../backend/models/renderer/cloze_renderer');
+const assert = require('assert').strict;
+
+
+
+describe('cloze_renderer', function () {
+    it('should render basic cloze cards', function () {
+        const card = {
+            fields: {
+                n: '{{c1::test}}'
+            }
+        };
+        const style = {
+            template: {
+                front: '{{cloze:n}}',
+                back: '{{cloze:n}}',
+            }
+        };
+        assert.deepStrictEqual(
+            ClozeRenderer.renderCards(card, style),
+            {
+                'Cloze 1': {
+                    front: '__***[..]***__',
+                    back: '__**n**__'
+                }
+            }
+        );
+    });
+});
