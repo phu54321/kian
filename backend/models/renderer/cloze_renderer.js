2fad95cf3a1fcb54d2279ca5c379bfc999cbf06a
phu54321 <phu54321@naver.com>
2018-07-27 06:19:45 +0900

    support multiple cloze field names

--- backend/models/renderer/cloze_renderer.js
+++ backend/models/renderer/cloze_renderer.js
@@ -1,29 +1,48 @@
 module.exports = {
     renderCards (card, style) {
         const fields = card.fields;
+        const {front: frontMarkup, back: backMarkup} = style.template[0];
 
-        const nField = fields.n;
-        const foundFragmentSet = {};
-        const frontFragmentSet = {};
-        const backFragmentSet = {};
-        nField.replace(/{{c(\d+)::(.+?)}}/g, function (match, p1, _p2) {
-            match = parseInt(p1);
-            if(foundFragmentSet[match]) return;
-            foundFragmentSet[match] = true;
-            frontFragmentSet[match] = nField.replace(new RegExp(`{{c${p1}::(.+?)}}`,'g'), function (_match, _p1) {
-                return '__***[..]***__';
-            });
-            backFragmentSet[match] = nField.replace(new RegExp(`{{c${p1}::(.+?)}}`,'g'), function (_match, p1) {
-                return `__**${p1}**__`;
-            });
+        const clozeFields = new Set;
+        const fragmentMap = {};
+        frontMarkup.replace(/{{cloze:(.+?)}}/g, function (match, p1) {
+            clozeFields.add(p1);
         });
 
+        const clozeIndexWithFragments = new Set;
+        clozeFields.forEach(fieldName => {
+            const field = fields[fieldName];
+            const foundFragmentSet = new Set;
+            const frontFragmentMap = {};
+            const backFragmentMap = {};
+            field.replace(/{{c(\d+)::(.+?)}}/g, function (match, p1, _p2) {
+                match = parseInt(p1);
+                if(foundFragmentSet.has(match)) return;
+                foundFragmentSet.add(match);
+                clozeIndexWithFragments.add(match);
+                frontFragmentMap[match] = field.replace(new RegExp(`{{c${p1}::(.+?)}}`,'g'), function (_match, _p1) {
+                    return '__***[..]***__';
+                });
+                backFragmentMap[match] = field.replace(new RegExp(`{{c${p1}::(.+?)}}`,'g'), function (_match, p1) {
+                    return `__**${p1}**__`;
+                });
+            });
+            fragmentMap[fieldName] = {
+                front: frontFragmentMap,
+                back: backFragmentMap,
+            };
+        });
+
+
         const ret = {};
-        const {front: frontMarkup, back: backMarkup} = style.template[0];
-        Object.keys(foundFragmentSet).forEach(k => {
+        clozeIndexWithFragments.forEach(k => {
             ret[`Cloze ${k}`] = {
-                front: frontMarkup.replace('{{cloze:n}}', frontFragmentSet[k]),
-                back: backMarkup.replace('{{cloze:n}}', backFragmentSet[k]),
+                front: frontMarkup.replace(/{{cloze:(.+?)}}/g, function (match, p1) {
+                    return fragmentMap[p1].front[k];
+                }),
+                back: backMarkup.replace(/{{cloze:(.+?)}}/g, function (match, p1) {
+                    return fragmentMap[p1].back[k];
+                }),
             };
         });
         return ret;
--- tests/test_cloze_renderer.js
+++ tests/test_cloze_renderer.js
@@ -4,16 +4,39 @@ const assert = require('assert').strict;
 
 
 describe('cloze_renderer', function () {
-    it('should render basic cloze cards', function () {
+    it('should render basic cloze cards (name n)', function () {
         const card = {
             fields: {
-                n: '{{c1::test}}'
+                n: '{{c1::test}}',
             }
         };
         const style = {
             template: [{
                 front: '{{cloze:n}}',
                 back: '{{cloze:n}}',
+            }]
+        };
+        assert.deepStrictEqual(
+            ClozeRenderer.renderCards(card, style),
+            {
+                'Cloze 1': {
+                    front: '__***[..]***__',
+                    back: '__**test**__'
+                }
+            }
+        );
+    });
+
+    it('should render basic cloze cards (name m)', function () {
+        const card = {
+            fields: {
+                m: '{{c1::test}}',
+            }
+        };
+        const style = {
+            template: [{
+                front: '{{cloze:m}}',
+                back: '{{cloze:m}}',
             }]
         };
         assert.deepStrictEqual(
