f332fbeb78e98119cff0d7997a3380e0234e97c2
phu54321 <phu54321@naver.com>
2018-08-01 04:49:10 +0900

    conversion to subtree - wip

--- backend/frontend/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const root = 'http://localhost:5000/'
-
-export async function getKian(apiPath) {
-    const response = (await axios.get(root + apiPath)).data;
-    if(response.error) throw new Error(response.error.toString());
-    return response.result;
-}
--- backend/poi.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    html: {
-        title: 'Kian',
-        description: 'Spaced learning app'
-    },
-    presets: [
-        require('poi-preset-eslint')( /* options */ )
-    ]
-};
