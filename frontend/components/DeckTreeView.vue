1da0a9adc969a4f134d9d9ddab3fdc24796e8487
phu54321 <phu54321@naver.com>
2018-08-02 00:04:33 +0900

    better deck view

--- frontend/components/DeckTreeView.vue
+++ frontend/components/DeckTreeView.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
-    b-list-group
+    div
         template(v-for='deck in sortedDeckTree')
-            b-list-group-item(:key='deck.name')
-                | {{deck.name}}
+            div.deck-row(:key='deck.name')
+                span.ml-4(v-for='n in indent')
+                template(v-if='deck.subDecks.length')
+                    icon.mr-2(v-if='deck.collapsed', name='regular/plus-square', scale=0.7)
+                    icon.mr-2(v-else, name='regular/minus-square', scale=0.7)
+                icon.mr-2(v-else, name='minus', scale=0.7)
+                span {{deck.name}}
                 div.float-right
                         span.new {{deck.newCount}}
                         | &nbsp;
                         span.rev {{deck.lrnCount + deck.revCount}}
-            deck-tree-view.ml-3(:tree='deck.subDecks', :indent='indent + 1')
+            deck-tree-view(:v-if='!deck.collapsed', tree='deck.subDecks', :indent='indent + 1')
     
 </template>
 
-<script>
-export default {
+<script>export default {
     props: ['tree', 'indent'],
     computed: {
         sortedDeckTree () {
@@ -26,6 +30,10 @@ export default {
 </script>
 
 <style scoped>
+.deck-row:hover {
+    background-color: #eee;
+}
+
 .new {
     color: #33f;
 }
