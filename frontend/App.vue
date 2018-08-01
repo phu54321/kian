18c66bd116ae387e75ed74be6dd75f065c8fc135
phu54321 <phu54321@naver.com>
2018-08-02 06:03:35 +0900

    Better overall styling

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,19 +1,21 @@
 <template lang='pug'>
-b-container
+div
     nprogress-container
     b-navbar(toggleable='md', variant='primary', type='dark')
-        b-navbar-toggle(target='nav_collapse')
-        b-navbar-brand(href='#') Kian
+        b-container
+            b-navbar-toggle(target='nav_collapse')
+            b-navbar-brand(href='#') Kian
 
-        b-navbar-nav.ml-auto
-            b-nav-item(to='/decks', v-b-tooltip.hover, title='Home')
-                icon(name='home')
-            b-nav-item(to='/browse', v-b-tooltip.hover, title='Browse')
-                icon(name='search')
-            b-nav-item(to='/stats', v-b-tooltip.hover, title='Statistics')
-                icon(name='chart-bar')
+            b-navbar-nav.ml-auto
+                b-nav-item(to='/decks', v-b-tooltip.hover, title='Home')
+                    icon(name='home')
+                b-nav-item(to='/browse', v-b-tooltip.hover, title='Browse')
+                    icon(name='search')
+                b-nav-item(to='/stats', v-b-tooltip.hover, title='Statistics')
+                    icon(name='chart-bar')
 
-    router-view
+    b-container.mt-3
+        router-view
 </template>
 
 <script>
