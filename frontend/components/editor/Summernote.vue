2023d33333f03239034eb7ff35d1e084bb14cac6
phu54321 <phu54321@naver.com>
2018-08-04 10:33:35 +0900

    basic 'wrap' function for summernote

--- frontend/components/editor/Summernote.vue
+++ frontend/components/editor/Summernote.vue
@@ -5,6 +5,18 @@
 
 import $ from 'jquery';
 
+function wrap (context, front, back) {
+    const range = context.invoke('createRange');
+    const frag = range.nativeRange().cloneContents();
+    const span = document.createElement('span');
+    span.appendChild(frag);
+    const oldHtml = span.innerHTML;
+    const match = oldHtml.match(/^(\s*)([^]*?)(\s*)$/);
+    const newHtml = match[1] + front + match[2] + back + match[3];
+    context.invoke('pasteHTML', newHtml);
+}
+
+
 export default {
     props : {
         value: {
@@ -17,15 +29,30 @@ export default {
     },
     mounted () {
         $(this.$el).summernote({
-            // toolbar: [],
             toolbar: [
-                ['style', ['bold', 'italic', 'underline', 'clear']],
-                ['font', ['strikethrough', 'superscript', 'subscript']],
-                ['fontsize', ['fontsize']],
+                ['style', ['style']],
+                ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
                 ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']]
+                ['para', ['ul', 'ol', 'paragraph', 'table', 'link', 'picture']],
+                ['misc', ['fullscreen', 'codeview', 'help', 'cloze']]
             ],
+            buttons: {
+                cloze (context) {
+                    const ui = $.summernote.ui;
+                    // create button
+                    const button = ui.button({
+                        contents: '<i class="fa fa-child"/> Hello',
+                        tooltip: 'hello',
+                        click: function () {
+                            // invoke insertText method with 'hello' on editor module.
+                            wrap(context, '{{', '}}');
+                            // context.invoke('editor.insertText', 'hello');
+                        }
+                    });
+
+                    return button.render();
+                }
+            },
             callbacks: {
                 onInit: () => {
                     $(this.$el).summernote('code', this.value);
