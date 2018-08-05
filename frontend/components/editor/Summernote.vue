b26583a7d20e9e47f538a572787b52b759577885
phu54321 <phu54321@naver.com>
2018-08-05 20:32:37 +0900

    Better cloze handling

--- frontend/components/editor/Summernote.vue
+++ frontend/components/editor/Summernote.vue
@@ -6,17 +6,53 @@
 import $ from 'jquery';
 
 
-function wrap (context, front, back) {
-    const range = context.invoke('createRange');
-    const frag = range.nativeRange().cloneContents();
-    const span = document.createElement('span');
-    span.appendChild(frag);
-    const oldHtml = span.innerHTML;
-    const match = oldHtml.match(/^(\s*)([^]*?)(\s*)$/);
-    const newHtml = match[1] + front + match[2] + back + match[3];
-    context.invoke('pasteHTML', newHtml);
+
+function fragFromHtml (html) {
+    const tmpDiv = document.createElement('div');
+    tmpDiv.innerHTML = html;
+    const frag = document.createDocumentFragment();
+    let node;
+    while((node = tmpDiv.firstChild)) {
+        frag.appendChild(node);
+    }
+    return frag;
 }
 
+function wrap (front, back) {
+    var s = window.getSelection();
+    var r = s.getRangeAt(0);
+    var content = r.extractContents();
+
+    var span = document.createElement('span');
+    span.appendChild(content);
+    const oldHtml = span.innerHTML;
+
+    if (oldHtml) {
+        var match = oldHtml.match(/^(\s*)([^]*?)(\s*)$/);
+        var newHtml = match[1] + front + match[2] + back + match[3];
+        var frag = fragFromHtml(newHtml);
+        
+        r.deleteContents();
+        r.insertNode(frag);
+        r.collapse();
+    }
+    else {
+        r.insertNode(fragFromHtml(front));
+        r.collapse();
+        r.insertNode(fragFromHtml(back));
+        r.collapse(true);
+    }
+}
+
+
+function getLastClozeId (code){
+    let maxClozeId = 0;
+    code.replace(/\{\{c(\d+)::/g, (match, g1) => {
+        const clozeId = parseInt(g1);
+        if (maxClozeId < clozeId) maxClozeId = clozeId;
+    });
+    return maxClozeId;
+}
 
 
 export default {
@@ -55,12 +91,11 @@ export default {
                         click: function () {
                             // invoke insertText method with 'hello' on editor module.
                             const code = context.invoke('code');
-                            let maxClozeId = 0;
-                            code.replace(/\{\{c(\d+)::/g, (match, g1) => {
-                                const clozeId = parseInt(g1);
-                                if (maxClozeId < clozeId) maxClozeId = clozeId;
-                            });
-                            wrap(context, `{{c${maxClozeId + 1}::`, '}}');
+                            const lastClozeId = getLastClozeId(code);
+                            const thisClozeId = lastClozeId + 1;
+                            context.invoke('beforeCommand');
+                            wrap(`{{c${thisClozeId}::`, '}}');
+                            context.invoke('afterCommand');
                         }
                     });
 
