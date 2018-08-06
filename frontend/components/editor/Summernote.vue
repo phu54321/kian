4ec096afa2577d0de478deb2367d259a978def85
phu54321 <phu54321@naver.com>
2018-08-06 16:53:16 +0900

    Removed toolbar from summerNote.vue

--- frontend/components/editor/Summernote.vue
+++ frontend/components/editor/Summernote.vue
@@ -19,6 +19,7 @@ export default {
     mounted () {
         $(this.$el).summernote({
             prettifyHtml: true,
+            toolbar: [],
             codemirror: { // codemirror options
                 theme: 'monokai',
                 mode: 'text/html',
