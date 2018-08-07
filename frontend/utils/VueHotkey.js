4f0a7f0ffa8203ae44ee4d509866b32746721a47
phu54321 <phu54321@naver.com>
2018-08-07 16:40:11 +0900

    added up modifierto v-hotkey

--- frontend/utils/VueHotkey.js
+++ frontend/utils/VueHotkey.js
@@ -1,25 +1,45 @@
 import {INVERSE_KEY_MAP} from './keycode';
 
+function eventToKeySequence (e) {
+    const s = [];
+    if (e.ctrlKey || e.metaKey) s.push('CTRL');
+    if (e.shiftKey) s.push('SHIFT');
+    if (e.altKey) s.push('ALT');
+    if (e.keyCode) s.push(INVERSE_KEY_MAP[e.keyCode]);
+    return s.join('+');
+}
+
 export default {
     install (Vue) {
         Vue.directive('hotkey', {
             bind (el, binding, _vnode, _oldVnode) {
                 const appliableKey = binding.value.map(x => x.toUpperCase());
-                el._keyHandler = function (e) {
-                    const s = [];
-                    if (e.ctrlKey || e.metaKey) s.push('CTRL');
-                    if (e.shiftKey) s.push('SHIFT');
-                    if (e.altKey) s.push('ALT');
-                    if (e.keyCode) s.push(INVERSE_KEY_MAP[e.keyCode]);
-                    const hotkeyString = s.join('+');
-                    if(appliableKey.indexOf(hotkeyString) != -1) {
-                        el.dispatchEvent(new Event('click'));
-                    }
+                if(binding.modifiers.up) {
+                    let lastPressedKeySequence = null;
+                    el._keyDownHandler = function (e) {
+                        lastPressedKeySequence = eventToKeySequence(e);
+                    };
+                    el._keyUpHandler = function () {
+                        if (appliableKey.indexOf(lastPressedKeySequence) != -1) {
+                            el.dispatchEvent(new Event('click'));
+                        }
+                        lastPressedKeySequence = null;
+                    };
                 }
-                document.addEventListener('keydown', el._keyHandler);
+                else {
+                    el._keyDownHandler = function (e) {
+                        if (appliableKey.indexOf(eventToKeySequence(e)) != -1) {
+                            el.dispatchEvent(new Event('click'));
+                        }
+                    };
+                    el._keyUpHandler = function () {};
+                }
+                document.addEventListener('keydown', el._keyDownHandler);
+                document.addEventListener('keyup', el._keyUpHandler);
             },
             unbind (el, _binding, _vnode, _oldVnode) {
-                document.removeEventListener('keydown', el._keyHandler);
+                document.removeEventListener('keydown', el._keyDownHandler);
+                document.removeEventListener('keyup', el._keyUpHandler);
             }
         });
     }
