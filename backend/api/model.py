aa19e5e7f6e0934a72caf6579a0dffc8d0c0e4e6
phu54321 <phu54321@naver.com>
2018-08-12 07:43:42 +0900

    model_get api

--- backend/api/model.py
+++ backend/api/model.py
@@ -8,3 +8,28 @@ from .dispatchTable import registerApi
 def getModelList(msg):
     with Col() as col:
         return emit.emitResult(col.models.allNames())
+
+@registerApi('model_get')
+def getModel(msg):
+    with Col() as col:
+        modelName = msg['modelName']
+        model = col.models.byName(modelName)
+        return emit.emitResult({
+            'type': 'basic' if model['type'] == 0 else 'cloze',
+            'name': model['name'],
+            'templates': [
+                {
+                    'name': template['name'],
+                    'front': template['qfmt'],
+                    'back': template['afmt'],
+                } for template in model['tmpls']
+            ],
+            'css': model['css'],
+            'fieldFormats': [
+                {
+                    'name': fFormat['name'],
+                    'sticky': fFormat['sticky'],
+                } for fFormat in model['flds']
+            ]
+        })
+    
