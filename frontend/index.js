51065ce9f64b8334924a939b0772345f6e9b2aff
phu54321 <phu54321@naver.com>
2018-08-12 08:08:10 +0900

    Default value for listSelector

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue';
 
 // Bootstrap
-import BootstrapVue from 'bootstrap-vue';
-Vue.use(BootstrapVue);
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
 
 import 'vue-awesome/icons';
 import Icon from 'vue-awesome/components/Icon.vue';
 Vue.component('icon', Icon);
 
-// Router
 import VueRouter from 'vue-router';
 import router from './router';
 Vue.use(VueRouter);
@@ -19,11 +17,12 @@ Vue.use(VueRouter);
 import VueSimpleHotkey from './utils/VueSimpleHotkey';
 Vue.use(VueSimpleHotkey);
 
-// Axios
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-
 Vue.use(VueAxios, axios);
+
+import vSelect from 'vue-select';
+Vue.component('v-select', vSelect);
 
 // App
 import App from './App';
