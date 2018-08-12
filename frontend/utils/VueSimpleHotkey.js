8d301e6d3be2061dad91a64be1f2909b1f351bf8
phu54321 <phu54321@naver.com>
2018-08-13 07:26:45 +0900

    v-hotkey: down by default,

--- frontend/utils/VueSimpleHotkey.js
+++ frontend/utils/VueSimpleHotkey.js
@@ -16,7 +16,18 @@ function registerHotkey (el, binding) {
     const appliableKey = hotkeyList.map(x => x.toUpperCase());
     const eventType = ['hotkey', 'click'];
 
-    if (binding.modifiers.up) {
+    if (binding.modifiers.down) {
+        el._keyDownHandler = function (e) {
+            if (appliableKey.indexOf(eventToKeySequence(e)) != -1) {
+                eventType.forEach(evt => {
+                    el.dispatchEvent(new Event(evt));
+                });
+                e.stopPropagation();
+                e.preventDefault();
+            }
+        };
+        el._keyUpHandler = function () {};
+    } else {
         let lastPressedKeySequence = null;
         el._keyDownHandler = function (e) {
             lastPressedKeySequence = eventToKeySequence(e);
@@ -29,17 +40,6 @@ function registerHotkey (el, binding) {
             }
             lastPressedKeySequence = null;
         };
-    } else {
-        el._keyDownHandler = function (e) {
-            if (appliableKey.indexOf(eventToKeySequence(e)) != -1) {
-                eventType.forEach(evt => {
-                    el.dispatchEvent(new Event(evt));
-                });
-                e.stopPropagation();
-                e.preventDefault();
-            }
-        };
-        el._keyUpHandler = function () {};
     }
     document.addEventListener('keydown', el._keyDownHandler, true);
     document.addEventListener('keyup', el._keyUpHandler);
@@ -48,7 +48,7 @@ function registerHotkey (el, binding) {
 
 function unregisterHotkey (el) {
     if (el._keyDownHandler) {
-        document.removeEventListener('keydown', el._keyDownHandler);
+        document.removeEventListener('keydown', el._keyDownHandler, true);
         document.removeEventListener('keyup', el._keyUpHandler);
         el._keyDownHandler = undefined;
         el._keyUpHandler = undefined;
