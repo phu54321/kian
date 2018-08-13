507599d96938780588b768c9d195ad4e93a35dc3
phu54321 <phu54321@naver.com>
2018-08-14 07:09:31 +0900

    Now uses socket.io backend

--- frontend/api/ankiCall.js
+++ frontend/api/ankiCall.js
@@ -1,12 +1,36 @@
-import axios from 'axios';
+import io from 'socket.io-client';
 
-const root = 'http://localhost:28735/';
+const apiRoot = 'http://localhost:28735/';
 
-export async function ankiCall (apiType, data) {
-    const response = (await axios.post(root, {
-        apiType,
-        ...data
-    })).data;
-    if(response.error) throw new Error(response.error.toString());
-    return response.result;
+const socket = io(apiRoot);
+
+const syncKeyHeader = Math.random().toString();
+let lastSyncKey = 0;
+
+function createSyncKey () {
+    return `syncKey_${syncKeyHeader}_${lastSyncKey++}`;
 }
+
+const msgTable = {};
+
+socket.on('msg', response => {
+    const syncKey = response.syncKey;
+    const { resolve, reject } = msgTable[syncKey];
+    msgTable[syncKey] = undefined;
+
+    if(response.error) return reject(new Error(response.error.toString()));
+    return resolve(response.result);
+});
+
+export function ankiCall (apiType, data) {
+    return new Promise((resolve, reject) => {
+        const syncKey = createSyncKey();
+        msgTable[syncKey] = { resolve, reject };
+        socket.emit('msg', {
+            syncKey,
+            apiType,
+            ...data
+        });    
+    });
+}
+
