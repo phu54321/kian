10c5d8f345feb0a889f652781a28fc1ca00258aa
phu54321 <phu54321@naver.com>
2018-08-14 03:11:28 +0900

    hotkey uses jquery.hotkeys & supports multiselect & a tag

--- frontend/utils/VueSimpleHotkey.js
+++ frontend/utils/VueSimpleHotkey.js
@@ -1,40 +1,65 @@
-import hotkeys from 'hotkeys-js';
 import $ from 'jquery';
+import 'jquery.hotkeys';
 
-function clickElement (el) {
+const classRules = {
+    multiselect: (el) => el.focus(),
+};
+
+const tagNameRules = {
+    a: (el) => el.click(),
+};
+
+function triggerHotkey (el, _binding, _vnode) {
     const $el = $(el);
     const {left, top} = $el.offset();
     const width = $el.width();
     const height = $el.height();
-    const targetEl = document.elementFromPoint(left + width / 2, top + height / 2);
+    let targetEl = document.elementFromPoint(left + width / 2, top + height / 2);
 
-    console.log(el, targetEl);
-    targetEl.dispatchEvent(new MouseEvent('mousedown'));
-    targetEl.dispatchEvent(new MouseEvent('mouseup'));
-    targetEl.dispatchEvent(new MouseEvent('click'));
+    for(;;) {
+        const $target = $(targetEl);
+        Object.keys(classRules).forEach(cls => {
+            if ($target.hasClass(cls)) {
+                classRules[cls](targetEl);
+                return;
+            }
+        });
+
+        const targetTagName = targetEl.tagName.toLowerCase();
+        const nameRule = tagNameRules[targetTagName];
+        if(nameRule) {
+            nameRule(targetEl);
+            return;
+        }
+
+        if(targetEl === el) break;
+        targetEl = targetEl.parentElement;
+    }
 }
 
-function registerHotkey (el, binding) {
-    if (el._keyDownHandler) unregisterHotkey(el);
+function registerHotkey (el, binding, vnode) {
+    if (el._onKeyDown) unregisterHotkey(el);
 
     let hotkeyList = binding.value;
     if (typeof hotkeyList === 'string') hotkeyList = [hotkeyList];
-    const hotkeyString = hotkeyList.map(x => x.toLowerCase()).join(',');
-    el._hotkeyString = hotkeyString;
- 
-    hotkeys(hotkeyString, function (e) {
-        clickElement(el);
-        console.log('click', hotkeyString, el);
+    const hotkeyString = hotkeyList.map(x => x.toLowerCase());
+
+    function onKeyDown (e) {
+        triggerHotkey(el, binding, vnode);
         e.stopPropagation();
         e.preventDefault();
-    });
+    }
 
+    el._onKeyDown = onKeyDown;
+    hotkeyString.forEach(kString => {
+        $(document).bind('keydown', kString, el._onKeyDown);
+    });
 }
 
 function unregisterHotkey (el) {
-    if (el._hotkeyString) {
-        hotkeys.unbind(el._hotkeyString);
-        el._hotkeyString = undefined;
+    if (el._onKeyDown) {
+        $(document).unbind('keydown', el._onKeyDown);
+        el._onKeyDown = undefined;
     }
 }
 
