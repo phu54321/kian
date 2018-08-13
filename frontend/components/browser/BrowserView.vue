28b2ab5329392e1935662ee5f1a57c9ab850ef8f
phu54321 <phu54321@naver.com>
2018-08-14 05:01:38 +0900

    Use b-table instead.

--- frontend/components/browser/BrowserView.vue
+++ frontend/components/browser/BrowserView.vue
@@ -1,35 +1,11 @@
 <template lang="pug">
 
-table.table.table-sm
-    thead
-        tr
-            th Front
-            th Deck
-            th Model
-            th #
-            th Created
-            th Updated
-            th Due
-            th Tags
-    tbody
-        template(v-for='item in cards')
-            tr(:key='item.id')
-                td
-                    .browser-col-preview {{textVersionjs(item.front)}}
-                td
-                    div {{item.deck}}
-                td
-                    .browser-col-model {{item.model}}
-                td
-                    div {{item.ord + 1}}
-                td
-                    div {{timeToText(item.createdAt)}}
-                td
-                    div {{timeToText(item.updatedAt)}}
-                td
-                    div {{timeToText(item.due)}}
-                td
-                    div {{item.tags.join(', ')}}
+div
+    b-table(
+        small, 
+        :sort-by.sync="sortBy"
+        :sort-desc.sync="sortDesc"
+        :items='cards', :fields='fields')
 
 </template>
 
@@ -37,19 +13,33 @@ table.table.table-sm
 
 import { ankiCall } from '../../api/ankiCall';
 import asyncData from '../../utils/asyncData';
-import textVersionjs from 'textVersionjs';
+import textVersionJs from 'textVersionjs';
 import padLeft from 'pad-left';
 
 export default {
     props: ['query'],
     data () {
         return {
+            sortBy: 'createdAt',
+            sortDesc: false,
             cards: [],
         };
     },
+    computed: {
+        fields: () => [
+            { label: 'Preview', key: 'front', sortable: true, formatter: 'textVersionJs', class: 'ellipsis' },
+            { label: 'Deck', key: 'deck', sortable: true },
+            { label: 'Model', key: 'model', sortable: true },
+            { label: '#', key: 'ord' },
+            { label: 'Created', key: 'createdAt', sortable: true, formatter: 'timeToText' },
+            { label: 'Modified', key: 'updatedAt', sortable: true, formatter: 'timeToText' },
+            { label: 'Due', key: 'due', sortable: true, formatter: 'timeToText' },
+            { label: 'Tag', key: 'tags', sortable: true, formatter: 'concatTags', class: 'ellipsis' },
+        ],
+    },
     methods: {
-        textVersionjs (text) {
-            return textVersionjs(text, {
+        textVersionJs (text) {
+            return textVersionJs(text, {
                 imgProcess (src, alt) {
                     return '';
                 }
@@ -61,6 +51,9 @@ export default {
             const month = date.getMonth();
             const day = date.getDate();
             return `${year}-${padLeft(month, 2, '0')}-${padLeft(day, 2, '0')}`;
+        },
+        concatTags (tags) {
+            return tags.join(', ');
         },
     },
     mixins: [asyncData(async props => {
@@ -77,30 +70,14 @@ export default {
 
 <style lang="scss" scoped>
 
-.browser-ellipsis {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-}
-
-table {
-    td {
-        font-size: .8em;
-        div {
-            @extend .browser-ellipsis;
-        }
-    }
-
-    .browser-col-preview {
-        @extend .browser-ellipsis;
-    }
-
-    .browser-col-model {
-        @extend .browser-ellipsis;
-    }
-
-    .browser-col-tags {
-        @extend .browser-ellipsis;
+div /deep/ td {
+    font-size: .8em;
+    &.ellipsis {
+        max-width: 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
 }
+
 </style>
