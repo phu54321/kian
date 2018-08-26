f243283ca380c5bd353ca5102fa8df4b48c6a07a
phu54321 <phu54321@naver.com>
2018-08-26 23:58:49 +0900

    addTags & removeTags button

--- frontend/components/browser/BrowserToolModals.vue
+++ frontend/components/browser/BrowserToolModals.vue
@@ -2,13 +2,16 @@
 
 div
     b-modal(id='browserChangeDeck', title='Change deck', lazy, @ok='changeDeck')
-        list-selector(taggable, focused, title='Deck name', v-model='newDeckName', apiType='deck_list')
+        list-selector(taggable, focused, title='Deck name', v-model='deck', apiType='deck_list')
 
     b-modal(id='browserChangeModel', title='Change model', lazy, @ok='changeModel')
-        list-selector(focused, title='Model name', v-model='newModelName', apiType='model_list')
+        list-selector(focused, title='Model name', v-model='model', apiType='model_list')
 
     b-modal(id='browserAddTags', title='Add tags', lazy, @ok='addTags')
-        tag-editor(focused, v-model='tagList')
+        tag-editor(focused, v-model='tags')
+
+    b-modal(id='browserRemoveTags', title='Remove tags', lazy, @ok='removeTags')
+        tag-editor(focused, v-model='tags')
 
 
 </template>
@@ -27,30 +30,43 @@ export default {
     },
     data () {
         return {
-            newDeckName: '',
-            newModelName: '',
-            tagList: [],
+            deck: '',
+            model: '',
+            tags: [],
         };
     },
     methods: {
         async changeDeck () {
             await ankiCall('card_update_deck_batch', {
-                deck: this.newDeckName,
+                deck: this.deck,
                 cardIds: this.selected,
             });
-            this.newDeckName = '';
+            this.deck = '';
             this.$emit('updateView');
         },
         async changeModel () {
             await ankiCall('card_update_model_batch', {
-                model: this.newModelName,
+                model: this.model,
                 cardIds: this.selected,
             });
-            this.newModelName = '';
+            this.model = '';
             this.$emit('updateCardIds');
         },
         async addTags () {
-            console.log('addtags');
+            await ankiCall('card_add_tag_batch', {
+                tags: this.tags,
+                cardIds: this.selected,
+            });
+            this.tags = [];
+            this.$emit('updateView');
+        },
+        async removeTags () {
+            await ankiCall('card_remove_tag_batch', {
+                tags: this.tags,
+                cardIds: this.selected,
+            });
+            this.tags = [];
+            this.$emit('updateView');
         }
     }
 };
