8b565c0dec7d3698ad4830b9e310ea03d2d12d2c
phu54321 <phu54321@naver.com>
2018-08-30 19:25:03 +0900

    Applied licenses to everything

--- frontend/components/editor/Summernote/common.js
+++ frontend/components/editor/Summernote/common.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2018 Hyun Woo Park
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function fragFromHtml (html) {
     const tmpDiv = document.createElement('div');
     tmpDiv.innerHTML = html;
@@ -17,7 +32,7 @@ export function copyHtml () {
     const span = document.createElement('span');
     span.appendChild(content);
     const oldHtml = span.innerHTML;
-    
+
     return oldHtml;
 }
 
