19333abbb295b71b32895e87b5123c74e34df533
박현우 <phu54321@naver.com>
2018-09-10 19:44:55 +0900

    Responsive navbar

--- frontend/App.vue
+++ frontend/App.vue
@@ -20,25 +20,35 @@ div.app-body
     hotkey-map
 
     header
-        b-navbar(fixed='top', toggleable='sm', variant='primary', type='dark')
+        b-navbar(fixed='top', toggleable='sm', variant='dark', type='dark')
             b-container
                 b-navbar-brand(href='#', to='/')
                     img.mr-1.logo(src='logo.svg')
                     | kian
 
-                b-navbar-nav.ml-auto
-                    b-nav-item(v-b-modal.cheatsheet, v-b-tooltip.hover, title='Show cheatsheet')
-                        icon(name='regular/keyboard', scale='1.3')
-                    b-nav-item(v-b-tooltip.hover, title='Sync now', v-b-modal.syncModal)
-                        icon(name='sync')
-                    b-nav-item.ml-3(v-hotkey='"H"', to='/decks', v-b-tooltip.hover, title='Home')
-                        icon(name='home')
-                    b-nav-item(v-hotkey='"A"', to='/card/add', v-b-tooltip.hover, title='Add card')
-                        icon(name='plus')
-                    b-nav-item(v-hotkey='"B"', to='/browse', v-b-tooltip.hover, title='Browse')
-                        icon(name='search')
-                    b-nav-item(to='/stats', v-b-tooltip.hover, title='Statistics')
-                        icon(name='chart-bar')
+                b-navbar-toggle(target="nav_collapse")
+
+                b-collapse(is-nav, id="nav_collapse")
+                    b-navbar-nav.ml-auto
+                        b-nav-item(v-b-modal.cheatsheet, v-b-tooltip.hover, title='Show cheatsheet')
+                            icon(name='regular/keyboard', scale='1.3')
+                            span.d-sm-none.ml-2 Show cheatsheet
+                        b-nav-item(v-b-tooltip.hover, title='Sync now', v-b-modal.syncModal)
+                            icon(name='sync')
+                            span.d-sm-none.ml-2 Sync now
+                        b-nav-text.ml-3
+                        b-nav-item(v-hotkey='"H"', to='/decks', v-b-tooltip.hover, title='Home')
+                            icon(name='home')
+                            span.d-sm-none.ml-2 Home
+                        b-nav-item(v-hotkey='"A"', to='/card/add', v-b-tooltip.hover, title='Add card')
+                            icon(name='plus')
+                            span.d-sm-none.ml-2 Add cards
+                        b-nav-item(v-hotkey='"B"', to='/browse', v-b-tooltip.hover, title='Browse')
+                            icon(name='search')
+                            span.d-sm-none.ml-2 Browser cards
+                        b-nav-item(to='/stats', v-b-tooltip.hover, title='Statistics')
+                            icon(name='chart-bar')
+                            span.d-sm-none.ml-2 Statistics
 
     b-container.app-container
         router-view
