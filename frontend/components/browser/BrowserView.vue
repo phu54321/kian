b0642d93cbb30b63cfe37618d0796b65f66c5730
박현우 <phu54321@naver.com>
2018-09-11 11:46:59 +0900

    Uses card index instead of card ids in BrowserView.

--- frontend/components/browser/BrowserView.vue
+++ frontend/components/browser/BrowserView.vue
@@ -16,7 +16,7 @@
 <template lang="pug">
 
 div.browser-view
-    span(v-hotkey="'ctrl+a'", title='Select all cards', @click='selectAll')
+    span(v-hotkey="'ctrl+a'", title='Select all cards', @click='onSelectAll')
 
     table.table.table-sm
         thead(slot='before-content')
@@ -31,11 +31,11 @@ div.browser-view
         tbody
             template(v-if='visibleCards.length > 0')
                 template(v-for='(card, index) in visibleCards')
-                    tr.item-row(:class='{selected: cardSelected[card.id]}',
-                        @click.exact.prevent='selectOnly(index)'
-                        @click.shift.exact.prevent='selectSequential(index)'
-                        @click.ctrl.exact.prevent='selectAdd(index)'
-                        @click.meta.exact.prevent='selectAdd(index)'
+                    tr.item-row(:class='{selected: cardSelected[index]}',
+                        @click.exact.prevent='selectCardIndexOnly(index)'
+                        @click.shift.exact.prevent='onSelectSequential(index)'
+                        @click.ctrl.exact.prevent='onSelectAdd(index)'
+                        @click.meta.exact.prevent='onSelectAdd(index)'
                     )
                         td(v-for='field in fields', :class='field.class')
                             .item-row-div {{ getFormatter(field.formatter)(card[field.key]) }}
@@ -86,13 +86,14 @@ div.browser-view
 
 <script>
 
+import _ from 'lodash';
 import ankiCall from '~/api/ankiCall';
 import BrowserEditor from './BrowserEditor';
 import BrowserToolModals from './BrowserToolModals';
 import fieldFormatter from './fieldFormatter';
 
 
-const cardPerPage = 10;
+const cardPerPage = 100;
 
 
 export default {
@@ -117,31 +118,34 @@ export default {
             page: 1,
             updateView: 0,
             lastSelectedIndex: -1,
-            cardCache: {},
-            cardSelected: {},
+            cardCache: [],
+            cardSelected: [],
         };
+    },
+    created () {
+        this.resetCardCache();
     },
     watch: {
         updateView () {
-            this.clearCardCache();
+            this.resetCardCache();
             this.lastSelectedIndex = -1;
         },
         cardIds () {
-            this.clearCardCache();
+            this.resetCardCache();
         }
     },
     asyncComputed: {
         visibleCards: {
             async get () {
-                const newCardIds = this.visibleItems.filter(x => !this.cardCache[x]);
+                const newCardIndexes = this.visibleIndexes.filter(idx => !this.cardCache[idx]);
                 const newCards = await ankiCall('browser_get_batch', {
-                    cardIds: newCardIds
+                    cardIds: newCardIndexes.map(idx => this.cardIds[idx])
                 });
                 let newCardIdx = 0;
-                this.visibleItems.forEach(id => {
-                    if(!this.cardCache[id]) this.cardCache[id] = newCards[newCardIdx++];
+                this.visibleIndexes.forEach(idx => {
+                    if(!this.cardCache[idx]) this.cardCache[idx] = newCards[newCardIdx++];
                 });
-                const cards = this.visibleItems.map(x => this.cardCache[x]);
+                const cards = this.visibleIndexes.map(idx => this.cardCache[idx]);
                 return cards;
             },
             watch () {
@@ -166,27 +170,25 @@ export default {
 
         // Card selection
         selectedCardCount () {
-            return this.cardIds.filter(c => this.cardSelected[c]).length;
+            return this.cardSelected.filter(x => x).length;
         },
         selectedCardId () {
             if(this.selectedCardCount !== 1) return -1;
-            const cardId = this.cardIds.filter(c => this.cardSelected[c])[0].id;
-            return cardId;
+            return this.cardIds[this.cardSelected.indexOf(true)];
         },
         selectedCardList () {
-            return this.cardIds.filter(c => this.cardSelected[c]).map(x => x.id);
+            const {cardIds} = this;
+            return this.cardSelected
+                .map((x, i) => x ? cardIds[i] : null)
+                .filter(x => x !== null);
         },
 
-
         // Pagination
-        visibleItems () {
-            return this.cardIds.slice(this.page * cardPerPage - cardPerPage, this.page * cardPerPage);
+        visibleIndexes () {
+            return _.range(this.page * cardPerPage - cardPerPage, this.page * cardPerPage);
         },
     },
     methods: {
-        loadPage (newPage) {
-            this.page = newPage;
-        },
         issueSortBy (sortField) {
             let { sortBy, sortOrder } = this;
             if(sortBy === sortField) {
@@ -202,67 +204,64 @@ export default {
             this.$emit('update:sortBy', sortBy);
             this.$emit('update:sortOrder', sortOrder);
         },
+
         getFormatter (formatter) {
             if(formatter) return fieldFormatter[formatter];
             else return (x) => x;
         },
 
         // Selection
-        selectCardId (cardId, selected) {
-            if(!!this.cardSelected[cardId] !== selected) this.$set(this.cardSelected, cardId, selected);
+        selectCardIndex (cardIndex, selected) {
+            this.$set(this.cardSelected, cardIndex, selected);
         },
 
-        /**
-         * selectCardId incurs vue reactivity change for each assignment. To 'not' trigger reactivity multiple times,
-         * we need a way to modify multiple card's selectivity in a 'batch' sense. 
-         */
-        selectCardIdBatch (cardIds, selected) {
-            const newObj = Object.assign({}, this.cardSelected);
-            for(const id of cardIds) newObj[id] = selected;
-            this.cardSelected = newObj;
+        selectCardIndexBatch (cardIndexes, selected) {
+            for(const idx of cardIndexes) this.selectCardIndex(idx, selected);
         },
 
-        selectOnly (index) {
-            const newSelectedCardId = this.visibleCards[index].id;
-            const origSelect = this.cardSelected[newSelectedCardId];
-            this.selectCardIdBatch(this.cardIds, false);
-            this.selectCardId(newSelectedCardId, !origSelect);
+        selectCardIndexOnly (index) {
+            const origSelect = this.cardSelected[index];
+            this.selectAll(false);
+            this.selectCardIndex(index, !origSelect);
             this.lastSelectedIndex = index;
         },
-        selectSequential (index) {
+
+        selectAll (selected) {
+            this.cardSelected = new Array(this.cardIds.length).fill(selected);
+        },
+
+        onSelectSequential (index) {
             const {lastSelectedIndex} = this;
-            if(lastSelectedIndex === -1) return this.selectOnly(index);
+            if(lastSelectedIndex === -1) return this.selectCardIndexOnly(index);
             else {
                 if(lastSelectedIndex < index) {
                     for(let i = lastSelectedIndex + 1 ; i <= index ; i++) {
-                        this.selectCardId(this.visibleCards[i].id, true);
+                        this.selectCardIndex(i, true);
                     }
                 }
                 else {
                     for(let i = lastSelectedIndex - 1 ; i >= index ; i--) {
-                        this.selectCardId(this.visibleCards[i].id, true);
+                        this.selectCardIndex(i, true);
                     }
                 }
                 this.lastSelectedIndex = index;
             }
         },
-        selectAdd (index) {
-            const origSelect = this.cardSelected[this.visibleCards[index]];
-            this.selectCardId(this.visibleCards[index].id, !origSelect);
+        onSelectAdd (index) {
+            const origSelect = this.cardSelected[index];
+            this.selectCardIndex(index, !origSelect);
             if(!origSelect) this.lastSelectedIndex = index;
         },
-        selectAll () {
-            if(this.cardIds.every(cardId => this.cardSelected[cardId])) {
-                this.selectCardIdBatch(this.cardIds, false);
-            } else {
-                this.selectCardIdBatch(this.cardIds, true);
-            }
+
+        onSelectAll () {
+            if(this.cardSelected.every(x => x)) this.selectAll(false);
+            else this.selectAll(true);
             this.lastSelectedIndex = this.cardIds.length - 1;
         },
 
-        clearCardCache () {
-            this.cardCache = {};  // Clear cache
-            this.cardSelected = {};
+        resetCardCache () {
+            this.cardCache = new Array(this.cardIds.length).fill(null);  // Clear cache
+            this.cardSelected = new Array(this.cardIds.length).fill(false);
         }
     }
 };
