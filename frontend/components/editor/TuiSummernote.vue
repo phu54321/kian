694040d638d86e2821081abf25502805bdf02821
phu54321 <phu54321@naver.com>
2018-09-12 19:03:46 +0900

    TuiSummernote: Use tui-editor on markdown mode, else summernote

---
+++ frontend/components/editor/TuiSummernote.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+
+tui-editor(v-if='isMarkdown', :value='value', @input='v => $emit("input", v)')
+summernote(v-else, :value='value', @input='v => $emit("input", v)')
+
+</template>
+
+<script>
+
+import TuiEditor from './TuiEditor/TuiEditor';
+import Summernote from './Summernote/Summernote';
+
+export default {
+    props: ['value'],
+    computed: {
+        isMarkdown () {
+            return TuiEditor.isEditableHtml(this.value);
+        },
+    },
+    components: {
+        TuiEditor,
+        Summernote,
+    },
+};
+
+</script>
