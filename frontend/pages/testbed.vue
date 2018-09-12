73fc5b250105da15de61cabec577ca0fd3a0fd41
phu54321 <phu54321@naver.com>
2018-09-12 15:51:00 +0900

    Added testbed page.
    Testbed page to test various things. Here we will test the tui-editor
    component, but we may use it in other way.

---
+++ frontend/pages/testbed.vue
@@ -0,0 +1,79 @@
+// Copyright (C) 2018 Hyun Woo Park
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<template lang="pug">
+div
+    h1.mb-4 Testbed
+
+    | testwork
+
+    div(ref='mdEdit')
+
+</template>
+
+<script>
+
+import TuiEditor from 'tui-editor';
+
+import 'codemirror/lib/codemirror.css';
+import 'tui-editor/dist/tui-editor.css';
+import 'tui-editor/dist/tui-editor-contents.css';
+import 'highlight.js/styles/github.css';
+
+const initialMarkdown = `## DPLD의 일반론
+
+- Sx: {{c1::운동시 dyspnea, 마른 기침}}  
+- P/E: {{c2::Basal fine crackles _(부직포 뜯는 소리)_}}
+- 폐기능검사
+    - FVC: {{c3::≤70}}%
+    - TLC: {{c7::≤70}}%
+    - FEV1/FVc: {{c4::정상_/증가_}}
+    - DLco: {{c5::감소 _(확산능 감소)_}}
+- 폐기능장애: {{c6::제한::폐쇄/제한}}성`;
+
+
+export default {
+    data () {
+        return {
+            markdown: initialMarkdown,
+            editor: null,
+        };
+    },
+    mounted () {
+        this.editor = new TuiEditor({
+            el: this.$refs.mdEdit,
+            initialEditType: 'markdown',
+            initialValue: this.markdown,
+            previewStyle: 'vertical',
+            height: '800px'
+        });
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+/deep/ ul {
+    list-style-type: disc;
+    ul {
+        list-style-type: circle;
+        ul {
+            list-style-type: square;
+        }
+    }
+}
+
+</style>
