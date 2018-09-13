c87baa845da51fbe66d03a93cb68fcd768157511
phu54321 <phu54321@naver.com>
2018-09-13 18:30:33 +0900

    Re-added clozing for codemirror editor

--- frontend/components/editor/TuiEditor/cloze.js
+++ frontend/components/editor/TuiEditor/cloze.js
@@ -1,44 +1,33 @@
-import TuiEditor from 'tui-editor';
 import { getLastClozeId } from '../utils/cloze';
+import CodeMirror from 'codemirror';
 
-TuiEditor.defineExtension('cloze', function (editor) {
-    editor.addCommand('markdown', {
-        name: 'cloze',
-        exec (mde) {
-            const cm = mde.getEditor();
-            const selections = cm.getSelections();
-            const lastClozeId = getLastClozeId(cm.getValue());
-            const replacements = selections.map((sel, index) => `{{c${lastClozeId + index + 1}::${sel}}}`);
-            cm.replaceSelections(replacements);
+const cmds = CodeMirror.commands;
+cmds.cloze = function (cm) {
+    const selections = cm.getSelections();
+    const lastClozeId = getLastClozeId(cm.getValue());
+    const replacements = selections.map((sel, index) => `{{c${lastClozeId + index + 1}::${sel}}}`);
+    cm.replaceSelections(replacements);
 
-            cm.setSelections(cm.listSelections().map((sel, index) => {
-                if(!selections[index]) {
-                    sel.head.ch -= 2;
-                    sel.anchor.ch -= 2;
-                }
-                return sel;
-            }));
-            mde.focus();
+    cm.setSelections(cm.listSelections().map((sel, index) => {
+        if(!selections[index]) {
+            sel.head.ch -= 2;
+            sel.anchor.ch -= 2;
         }
-    });
+        return sel;
+    }));
+};
 
-    editor.addCommand('markdown', {
-        name: 'cloze-same',
-        exec (mde) {
-            const cm = mde.getEditor();
-            const selections = cm.getSelections();
-            const lastClozeId = getLastClozeId(cm.getValue()) || 1;
-            const replacements = selections.map((sel, index) => `{{c${lastClozeId}::${sel}}}`);
-            cm.replaceSelections(replacements);
+cmds.clozeSame = function (cm) {
+    const selections = cm.getSelections();
+    const lastClozeId = getLastClozeId(cm.getValue()) || 1;
+    const replacements = selections.map((sel, index) => `{{c${lastClozeId}::${sel}}}`);
+    cm.replaceSelections(replacements);
 
-            cm.setSelections(cm.listSelections().map((sel, index) => {
-                if(!selections[index]) {
-                    sel.head.ch -= 2;
-                    sel.anchor.ch -= 2;
-                }
-                return sel;
-            }));
-            mde.focus();
+    cm.setSelections(cm.listSelections().map((sel, index) => {
+        if(!selections[index]) {
+            sel.head.ch -= 2;
+            sel.anchor.ch -= 2;
         }
-    });
-});
+        return sel;
+    }));
+};
