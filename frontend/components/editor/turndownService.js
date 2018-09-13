90a1fec03af8f3e5e915eae6d359854820bac76d
phu54321 <phu54321@naver.com>
2018-09-13 17:57:12 +0900

    Basic codemirror settings
    tui.editor takes a while to initialize, so editing notes with dozens of fields
    may lag the editor. (ex: Cloze (overlapping))
    
    Codemirror takes much less to initialize, so I'm changing tui.editor to codemirror.

--- frontend/components/editor/turndownService.js
+++ frontend/components/editor/turndownService.js
@@ -4,5 +4,6 @@ const turndownService = new TurndownService();
 const gfm = require('turndown-plugin-gfm').gfm;
 turndownService.use(gfm);
 turndownService.keep(['font']);
+turndownService.remove(['script']);
 
 export default turndownService;
