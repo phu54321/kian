c07e8eff8733b1d6b065a445416b784fbaef8080
phu54321 <phu54321@naver.com>
2018-09-13 20:49:02 +0900

    Table generator: from PowerFormatPack-like syntax to markdown

---
+++ frontend/components/editor/TuiEditor/addons/table.js
@@ -0,0 +1,38 @@
+import { getLastClozeId } from '../../utils/cloze';
+import CodeMirror from 'codemirror';
+
+CodeMirror.commands.makeTable = function (cm) {
+    const tableData = cm.getSelection();
+
+    const lines = tableData.split('\n').filter(x => x);
+    const cells = lines.map(x => x.split('|').map(x => x.trim()));
+    const colCount = Math.max.apply(null, cells.map(x => x.length));
+    const rowCount = cells.length;
+
+    if (!(rowCount && colCount)) {
+        alert('Select some text');
+        return;
+    }
+    else if(rowCount === 1) {
+        alert('Header-only (single row) table not supported');
+        return;
+    }
+
+    const output = [];
+    for (let y = 0 ; y < rowCount ; y++) {
+        const row = cells[y];
+        while(row.length < colCount) row.push('');
+        output.push('|');
+        for (let x = 0 ; x < colCount ; x++) {
+            output.push(' ');
+            output.push(row[x]);
+            output.push(' |');
+        }
+        output.push('\n');
+        if(y === 0) {
+            output.push('|' + ' -- |'.repeat(colCount) + '\n');
+        }
+    }
+
+    cm.replaceSelection(output.join(''));
+};
