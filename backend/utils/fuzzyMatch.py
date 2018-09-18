bf0ba613649e42e7fea0651e15dc7bb8f794671f
phu54321 <phu54321@naver.com>
2018-09-18 11:26:05 +0900

    fuzzy matching added to python

---
+++ backend/utils/fuzzyMatch.py
@@ -0,0 +1,19 @@
+import hgtk
+
+
+def fuzzyMatch(needle, hasystack):
+    needle = hgtk.text.decompose(needle).lower()
+    hasystack = hgtk.text.decompose(hasystack).lower()
+
+    try:
+        idx = 0
+        print(needle, hasystack)
+        for ch in needle:
+            while True:
+                if hasystack[idx] == ch:
+                    break
+                idx += 1
+            idx += 1
+        return True
+    except IndexError:
+        return False
