a2126fe88e5259da57277b5906e7b91c9702464e
phu54321 <phu54321@naver.com>
2018-09-18 19:04:47 +0900

    markdown contents are encode with base64, to prevent them from accidently being searched by Anki's search engine.

--- frontend/components/editor/TuiEditor/decompiler/markdownDecompiler.js
+++ frontend/components/editor/TuiEditor/decompiler/markdownDecompiler.js
@@ -1,5 +1,7 @@
 import TurndownService from 'turndown';
 import crc32 from 'crc-32';
+import base64 from 'base-64';
+import utf8 from 'utf8';
 
 const turndownService = new TurndownService();
 const gfm = require('turndown-plugin-gfm').gfm;
@@ -24,16 +26,34 @@ export function turndown (html) {
     return turndownService.turndown(html);
 }
 
+function getMarkdownContent (domElement) {
+    let markdownElements;
+
+    markdownElements = domElement.getElementsByClassName('tui-md');
+    if(markdownElements.length === 1) return {
+        markdown: markdownElements[0].innerHTML,
+        expectedHtmlCRC: markdownElements[0].getAttribute('hash'),
+    };
+
+    try {
+        markdownElements = domElement.getElementsByClassName('tui-md-b64');
+        if(markdownElements.length === 1) return {
+            markdown: utf8.decode(base64.decode(markdownElements[0].innerHTML)),
+            expectedHtmlCRC: markdownElements[0].getAttribute('hash'),
+        };
+    } catch (e) {;}  // Error might occur during utf8.decode
+
+    return {};
+}
+
 export default function decodeMarkdown (html) {
     if(!html) return '';
 
     const parser = new DOMParser();
     const domElement = parser.parseFromString(html, 'text/html');
     
-    const markdownElements = domElement.getElementsByClassName('tui-md');
-    if(markdownElements.length !== 1) return null;
-    const markdown = markdownElements[0].innerHTML;
-    const expectedHtmlCRC = markdownElements[0].getAttribute('hash');
+    const { markdown, expectedHtmlCRC } = getMarkdownContent(domElement);
+    if(!markdown) return null;
 
     const htmlElement = domElement.getElementsByClassName('tui-html');
     if(htmlElement.length !== 1) return null;
