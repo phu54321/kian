15433cd844b5d2b6a65b60299d67115bfcbf465c
phu54321 <phu54321@naver.com>
2018-09-18 16:27:30 +0900

    Basic markdown-it-katex integration

--- frontend/components/editor/TuiEditor/renderer/markdownRenderer.js
+++ frontend/components/editor/TuiEditor/renderer/markdownRenderer.js
@@ -1,16 +1,19 @@
 import MarkdownIt from 'markdown-it';
-import KianComment from './comment';
-import crc32 from 'crc-32';
-
 const md = new MarkdownIt({ html: true, linkify: true, });
-KianComment(md);
+md.use(require('markdown-it-katex'), {
+    throwOnError: false,
+});
+md.use(require('./comment'));
+
+
+import crc32 from 'crc-32';
 
 const encoderDom = document.createElement('div');
 
 export default function encodeMarkdown (markdown) {
     if(markdown === '') return '';
 
-    let html = md.render(markdown);(markdown);
+    let html = md.render(markdown);
 
     // Note: Browser may apply its specific escaping rules when HTML really gets into DOM.
     // ( for instance, browser may remove unmatched opening/closing tags without warning )
@@ -20,5 +23,8 @@ export default function encodeMarkdown (markdown) {
     html = encoderDom.innerHTML;
 
     const htmlHash = crc32.str(html.trim());
-    return `<script class='tui-md' type='text/markdown' hash='${htmlHash}'>${markdown}</sc` + `ript><div class='tui-html'>${html}</div>`;
+    const additionalHtml = `
+        <link rel="stylesheet" type="text/css" href="_kian/katex/katex.min.css">
+    `;
+    return `<script class='tui-md' type='text/markdown' hash='${htmlHash}'>${markdown}</sc` + `ript>${additionalHtml}<div class='tui-html'>${html}</div>`;
 }
