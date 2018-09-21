760d019551cfc1d38c82761b6754dd32cddf3e53
phu54321 <phu54321@naver.com>
2018-09-22 02:54:19 +0900

    Basic deck api

---
+++ frontend/api/deck.js
@@ -0,0 +1,18 @@
+import ankiCall from './ankiCall';
+
+export async function listDeck () {
+    return ankiCall('deck_list');
+}
+
+export async function getDeckInfo (deckName) {
+    return ankiCall('deck_info', {
+        deckName
+    });
+}
+
+export function collapseDeck (deckName, collapse) {
+    return ankiCall('deck_collapse', {
+        deckName,
+        collapse,
+    });
+}
