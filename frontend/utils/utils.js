04a9a43f035ff17b3f66cf5f47083b1cdb0d0464
phu54321 <phu54321@naver.com>
2018-09-22 02:26:50 +0900

    Removed typescript.
    I lived well without one.

--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -1,18 +1,18 @@
-const fuzzysearch = require('fuzzysearch');
-const Hangul = require('hangul-js');
+import fuzzysearch from 'fuzzysearch';
+import Hangul from 'hangul-js';
 
-export function clamp (x: number, min: number, max: number) {
+export function clamp (x, min, max) {
     if (x < min) x = min;
     if (x > max) x = max;
     return x;
 }
 
-function joinArray (x: any) {
+function joinArray (x) {
     if (Array.isArray(x)) return x.join('');
     else return x;
 }
 
-export function fuzzyMatch (needle: string, haystack: string) {
+export function fuzzyMatch (needle, haystack) {
     needle = joinArray(Hangul.disassemble(needle));
     haystack = joinArray(Hangul.disassemble(haystack));
     return fuzzysearch(needle.toLowerCase(), haystack.toLowerCase());
@@ -25,10 +25,10 @@ export function focusNextElement () {
         input[type=text]:not([disabled]),
         [tabindex]:not([disabled]):not([tabindex="-1"])
     `;
-    const activeElement: HTMLFormElement = document.activeElement as HTMLFormElement;
+    const activeElement = document.activeElement;
     if (activeElement && activeElement.form) {
         const focussable = Array.prototype.filter.call(activeElement.form.querySelectorAll(focussableElements),
-            (element: HTMLElement) => {
+            (element) => {
                 return element.offsetWidth > 0 || element.offsetHeight > 0 || element === activeElement;
             });
         const index = focussable.indexOf(activeElement);
