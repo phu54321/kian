e319754f40e15077bfa1490f0b0d913776b39376
phu54321 <phu54321@naver.com>
2018-09-28 02:15:08 +0900

    added minipaint to public/

---
+++ public/minipaint/webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		'babel-polyfill',
+		'./src/js/main.js',
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/dist/'
+	},
+	resolve: {
+		extensions: ['.js', '.css'],
+		alias: {
+			Utilities: path.resolve(__dirname, './../node_modules/')
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {minimize: true, url: false}
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+				loader: 'babel-loader',
+					options: {
+					presets: ['env']
+					}
+				}
+			},
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+		}),
+		new webpack.DefinePlugin({
+			VERSION: JSON.stringify(require("./package.json").version)
+		}),
+	],
+	devtool: "cheap-module-source-map",
+	devServer: {
+		contentBase: "./",
+		compress: true,
+	}
+};
