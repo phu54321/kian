4a4affd6c8d52e1bab85d6bfaa46e0162121110b
phu54321 <phu54321@naver.com>
2018-09-28 02:04:24 +0900

    Use `nonReactiveCopy` instead of _.clone in CardEditor.vue
    For example, we needed a code like this to copy `this.internalValue.fields`
    instead of mutating it directly, because `this.internalValue.fields` is
    reactive. However, because `_.cloneDeep` ** won't ** create a non-reactive
    clone, we need a special `nonReactiveCopy` function.
    
    ```
    const newFields = _.clone(this.internalValue.fields);
    resize(newFields, fieldFormats.length, '');
    this.internalValue.fields = newFields;
    ```

---
+++ frontend/utils/nonReactiveCopy.js
@@ -0,0 +1,11 @@
+export default function nonReactiveCopy (v) {
+    if (typeof v !== 'object') return v;
+
+    if (Array.isArray(v)) return v.map(x => nonReactiveCopy(x));
+
+    const ret = {};
+    for (const key in v) {
+        ret[key] = nonReactiveCopy(v[key]);
+    }
+    return ret;
+}
