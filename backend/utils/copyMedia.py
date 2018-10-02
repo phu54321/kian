682d728b6d3781ee4e440a9876b5365e748ce275
phu54321 <phu54321@naver.com>
2018-10-02 22:18:45 +0900

    Flattened media folder
    Anki don't support directory structure inside media folder, so
    we flattened the directory structure

--- backend/utils/copyMedia.py
+++ backend/utils/copyMedia.py
@@ -4,23 +4,21 @@ import shutil
 
 
 def copyDirectoryRecursive(srcDir, dstDir):
-    """ shutil.copytree that overwrites existing contents"""
-    if not os.path.exists(dstDir):
-        os.mkdir(dstDir)
+    """ copy all contenst of srcDir and its subdirectory to dstDir.
 
+    Directory structure is flattened. """
     for name in os.listdir(srcDir):
         if name.startswith('.'):
             continue
         srcPath = os.path.join(srcDir, name)
-        dstPath = os.path.join(dstDir, name)
         if os.path.isdir(srcPath):
-            copyDirectoryRecursive(srcPath, dstPath)
+            copyDirectoryRecursive(srcPath, dstDir)
         else:
+            dstPath = os.path.join(dstDir, name)
             shutil.copy(srcPath, dstPath)
 
 
 def copyAllMedia():
     with Col() as col:
-        mediaDir = os.path.join(col.media.dir(), '_kian')
         srcDir = os.path.join(os.path.dirname(__file__), '../../media/')
-        copyDirectoryRecursive(srcDir, mediaDir)
+        copyDirectoryRecursive(srcDir, col.media.dir())
