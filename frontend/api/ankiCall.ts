2e9ed946ba0f2ba0f65cb023625e352b6d065e42
박현우 <phu54321@naver.com>
2018-10-19 18:30:03 +0900

    tslint standardjs

--- frontend/api/ankiCall.ts
+++ frontend/api/ankiCall.ts
@@ -13,48 +13,48 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import io from 'socket.io-client';
-import Vue from 'vue';
+import io from 'socket.io-client'
+import Vue from 'vue'
 
-const apiRoot = 'http://localhost:28735/';
-const socket = io(apiRoot);
+const apiRoot = 'http://localhost:28735/'
+const socket = io(apiRoot)
 
-const syncKeyHeader = Math.random().toString();
-let lastSyncKey = 0;
+const syncKeyHeader = Math.random().toString()
+let lastSyncKey = 0
 
 function createSyncKey () {
-    return `syncKey_${syncKeyHeader}_${lastSyncKey++}`;
+  return `syncKey_${syncKeyHeader}_${lastSyncKey++}`
 }
 
-const callbackTable = new Map();
+const callbackTable = new Map()
 
 interface IResponse {
-    syncKey: string;
-    [key: string]: any;
+  syncKey: string
+  [key: string]: any
 }
 
 socket.on('msg', (response: IResponse) => {
-    const { syncKey } = response;
-    const callback = callbackTable.get(syncKey);
-    if (!callback) return;
+  const { syncKey } = response
+  const callback = callbackTable.get(syncKey)
+  if (!callback) return
 
-    const { resolve, reject } = callback;
-    callbackTable.delete(syncKey);
+  const { resolve, reject } = callback
+  callbackTable.delete(syncKey)
 
-    if (response.error) return reject(new Error(response.error.toString()));
-    return resolve(response.result);
-});
+  if (response.error) return reject(new Error(response.error.toString()))
+  return resolve(response.result)
+})
 
 export default function ankiCall (apiType: string, data?: any) {
-    return new Promise<any>((resolve, reject) => {
-        const syncKey = createSyncKey();
-        callbackTable.set(syncKey, { resolve, reject });
-        socket.emit('msg', {
-            apiType,
-            syncKey,
-            ...data,
-        });
-    });
+  return new Promise<any>((resolve, reject) => {
+    const syncKey = createSyncKey()
+    callbackTable.set(syncKey, { resolve, reject })
+    socket.emit('msg', {
+      apiType,
+      syncKey,
+      ...data
+    })
+  })
 }
 
-Vue.prototype.$ankiCall = ankiCall;
+Vue.prototype.$ankiCall = ankiCall
