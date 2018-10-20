e2206353ee67b8f2aacc8b00ec35f3f7ad79d6fb
박현우 <phu54321@naver.com>
2018-10-20 15:42:21 +0900

    $ankiCall refactored to sync.ts

--- frontend/components/SyncDialog.vue
+++ frontend/components/SyncDialog.vue
@@ -37,7 +37,7 @@ b-modal(v-model='show', lazy, id='syncModal', title='Sync to AnkiWeb', @shown='o
         template(slot='modal-header')
             h4 Sync to AnkiWeb
             .float-right
-                b-badge(variant='primary') Sent: {{formatBytes(sendBytes)}}
+                b-badge(variant='primary') Sent: {{formatBytes(sentBytes)}}
                 b-badge.ml-2(variant='success') Recv: {{formatBytes(recvBytes)}}
         b-progress.mt-2(:value='100', :max='100', animated)
         ul.list-group(ref='messageContainer')
@@ -48,6 +48,7 @@ b-modal(v-model='show', lazy, id='syncModal', title='Sync to AnkiWeb', @shown='o
 <script>
 
 import ErrorDialog from './ErrorDialog'
+import { issueSync } from '@/api'
 
 export default {
   data () {
@@ -93,91 +94,39 @@ export default {
     },
     startSync () {
       this.isLoginForm = false
-      this.sendBytes = 0
+      this.syncMessages = []
+      this.sentBytes = 0
       this.recvBytes = 0
 
-      this.$ankiCall('sync', {
+      const auth = {
         email: this.email,
         password: this.password,
         authKey: this.authKey
-      }).then(() => {
-        this.syncTimeout = setTimeout(this.syncProcess, 1000)
-      }).catch(err => {
-        ErrorDialog.openErrorDialog('Sync failed', err.message)
-        this.show = false
-      })
-    },
-    syncProcess () {
-      this.$ankiCall('sync_status').then(msg => {
-        for (let message of msg.messages) {
-          this.processSyncMessage(message[0], message[1])
+      }
+      const callbacks = {
+        onNewAuthkey: (key) => {
+          if (key === null) this.$localStorage.remove('syncKey')
+          else this.$localStorage.set('syncKey', key)
+        },
+        onSyncMessage: (msg) => {
+          this.syncMessages.push(msg)
+        },
+        onTransferUpdate: (sent, recv) => {
+          this.sentBytes = sent
+          this.recvBytes = recv
         }
-
-        if (msg.completed) {
+        // TODO: Add Fullsync support
+      }
+      issueSync(auth, callbacks)
+        .then(() => {
           this.syncTimeout = null
           this.show = false
           window.onbeforeunload = undefined
           this.$router.go()
-        } else {
-          this.syncTimeout = setTimeout(this.syncProcess, 1000)
-        }
-      })
-    },
-    processSyncMessage (cmd, arg) {
-      if (cmd === 'badAuth') {
-        ErrorDialog.openErrorDialog('Sync failed', 'Invalid AnkiWeb ID/Password')
-        this.$localStorage.delete('syncKey')
-      } else if (cmd === 'newKey') {
-        this.$localStorage.set('syncKey', arg)
-      } else if (cmd === 'offline') {
-        this.$toasted.show('Internet offline')
-      } else if (cmd === 'upbad') {
-        this.syncMessages.push('Uploading failed.')
-      } else if (cmd === 'send') {
-        this.sendBytes = arg
-      } else if (cmd === 'recv') {
-        this.recvBytes = arg
-      } else if (cmd === 'sync') {
-        const type = arg[0]
-        switch (type) {
-          case 'login':
-            this.syncMessages.push('Logon to AnkiWeb')
-            break
-          case 'upload':
-            this.syncMessages.push('Performing full upload to AnkiWeb.')
-            break
-          case 'download':
-            this.syncMessages.push('Performing full download from AnkiWeb.')
-            break
-          case 'sanity':
-            this.syncMessages.push('Checking database.')
-            break
-          case 'findMedia':
-            this.syncMessages.push('Checking media.')
-            break
-          case 'upgradeRequited':
-            this.$toasted.show('Deck upgrade required. Please visit AnkiWeb.')
-            break
-        }
-      } else if (cmd === 'syncMsg') {
-        this.syncMessages.push(arg)
-      } else if (cmd === 'error') {
-        ErrorDialog.openErrorDialog('Sync failed', arg)
-      } else if (cmd === 'clockOff') {
-        ErrorDialog.openErrorDialog('Sync failed', 'Check again your computer\'s clock.')
-      } else if (cmd === 'checkFailed') {
-        ErrorDialog.openErrorDialog('Sync failed', 'Database check failed.')
-      } else if (cmd === 'noChanges') {
-        this.syncMessages.push('No database changes')
-      } else if (cmd === 'noMediaChanges') {
-        this.syncMessages.push('No media changes')
-      } else if (cmd === 'fullSync') {
-        this.fullSyncAsked = true
-      }
-    },
-    fullSyncOption (mode) {
-      this.fullSyncAsked = false
-      this.$ankiCall('sync_fullsync', { mode })
+        }).catch(err => {
+          ErrorDialog.openErrorDialog('Sync failed', err.message)
+          this.show = false
+        })
     }
   }
 }
