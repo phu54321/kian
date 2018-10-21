c606b8c78b7a615b5ed3e56eabdf9c0832fd696e
박현우 <phu54321@naver.com>
2018-10-21 15:29:35 +0900

    Browser-related subapi moved to browser.ts

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,6 +1,7 @@
 export * from './subapi/deck'
 export * from './subapi/model'
 export * from './subapi/card'
+export * from './subapi/browser'
 export * from './subapi/sched'
 export * from './subapi/autocomplete'
 export * from './subapi/media'
--- frontend/api/subapi/card.ts
+++ frontend/api/subapi/card.ts
@@ -9,6 +9,13 @@ interface INoteDef {
   tags: string
 }
 
+export enum SchedType {
+  New,
+  Learn,
+  Review,
+  NotScheduled
+}
+
 /**
  * Add note to databsae
  */
@@ -21,69 +28,6 @@ export async function addNote (noteDef: INoteDef) {
     fields,
     tags
   })
-}
-
-export async function queryCardIds (param?: {
-  query: string,
-  sortBy: string,
-  sortOrder: string
-}) {
-  const { query, sortBy, sortOrder }: {
-    query ?: string,
-    sortBy ?: string,
-    sortOrder ?: string
-  } = param || {}
-
-  return ankiCall('browser_query', {
-    query: query || '',
-    sortBy: sortBy || 'createdAt',
-    sortOrder: sortOrder || 'desc'
-  })
-}
-
-export enum SchedType {
-  New,
-  Learn,
-  Review,
-  NotScheduled
-}
-interface ICardBrowserInfo {
-  id: number
-  noteId: number
-  deck: string
-  model: string
-  ord: number
-  preview: string
-  tags: string[]
-  createdAt: number
-  updatedAt: number
-  due: number
-  schedType: SchedType
-  suspended: boolean
-}
-
-export async function getCardsBrowserInfo (cardIds: number[]): Promise<ICardBrowserInfo[]> {
-  const cards = await ankiCall('browser_get_batch', {
-    cardIds
-  })
-  return cards.map((card: any) => ({
-    id: card.id,
-    noteId: card.noteId,
-    deck: card.deck,
-    model: card.model,
-    ord: card.ord,
-    preview: card.preview,
-    tags: card.tags,
-    createdAt: card.createdAt,
-    updatedAt: card.updatedAt,
-    due: card.due,
-    schedType:
-    (card.schedType === 'new') ? SchedType.New :
-      (card.schedType === 'lrn') ? SchedType.Learn :
-      (card.schedType === 'rev') ? SchedType.Review :
-      SchedType.NotScheduled,
-    suspended: card.suspended
-  }))
 }
 
 export async function getCardById (cardId: number[] | number) {
