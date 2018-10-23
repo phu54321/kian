e52db1bd9b2aeb46178f2c0f9d4749bd8dedc25d
박현우 <phu54321@naver.com>
2018-10-23 20:02:28 +0900

    .eslintrc.js is now handled by eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
-const path = require('path');
-
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-        "mocha": true,
-    },
-    "extends": [
-        "eslint-config-standard",
-        'plugin:vue/essential',
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "parser": "typescript-eslint-parser",
-        "plugins": ["typescript"],
-        "ecmaVersion": 2017,
-        "sourceType": "module"
-    },
-};
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'eslint-config-standard',
+    'plugin:vue/essential'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'typescript-eslint-parser',
+    plugins: ['typescript'],
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  plugins: ['vue']
+}
