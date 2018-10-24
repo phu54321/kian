dc7631516eb46e14da31ccf54f0b97aef67d72e9
박현우 <phu54321@naver.com>
2018-10-24 21:13:48 +0900

    Removed obsolete frontend/ folder

--- frontend/api/subapi/autocomplete.ts
+++
@@ -1,9 +0,0 @@
-import ankiCall from '../ankiCall'
-
-export function autocompleteTag (query: string) {
-  return ankiCall('tag_suggestions', { query })
-}
-
-export function autocompleteWord (query: string) {
-  return ankiCall('get_word_autocomplete', { query })
-}
