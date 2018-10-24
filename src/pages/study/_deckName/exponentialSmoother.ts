7a7c59bbfee9025cf5b1409eb0972bca08ad5eed
박현우 <phu54321@naver.com>
2018-10-24 22:11:05 +0900

    toolbar, expSmoother typescriptized

--- src/pages/study/_deckName/exponentialSmoother.ts
+++ src/pages/study/_deckName/exponentialSmoother.ts
@@ -1,22 +1,29 @@
-function getCurrentTime () {
+function getCurrentTime (): number {
   return (new Date()).getTime() / 1000
 }
 
 const maxInterval = 120
 
-export default class ExponentialSmoother {
-  constructor () {
-    this.logs = []
-    this.startTime = this.lastTime = getCurrentTime()
-    this.elapsedTime = 0
-  }
+interface LogEntry {
+  timeSpent: number,
+  progress: number,
+}
 
-  update (newPercent) {
+export default class ExponentialSmoother {
+  logs: LogEntry[] = []
+  startTime = getCurrentTime()
+  lastTime = getCurrentTime()
+  elapsedTime = 0
+
+  update (newPercent: number) {
     const timeSpent = Math.min(
       getCurrentTime() - this.lastTime,
       maxInterval
     )
-    this.logs.push([timeSpent, newPercent])
+    this.logs.push({
+      timeSpent,
+      progress: newPercent
+    })
     this.logs = this.logs.slice(-200)
     this.lastTime = getCurrentTime()
     this.elapsedTime = getCurrentTime() - this.startTime
@@ -30,8 +37,8 @@ export default class ExponentialSmoother {
 
     for (let i = 1; i < this.logs.length; i++) {
       const r = Math.pow(1.005, i)
-      totTime += r * this.logs[i - 1][0]
-      percentChange += r * (this.logs[i][1] - this.logs[i - 1][1])
+      totTime += r * this.logs[i - 1].timeSpent
+      percentChange += r * (this.logs[i].progress - this.logs[i - 1].progress)
     }
     if (totTime < 1) return 1
     return Math.max(percentChange / totTime, 1e-6)
