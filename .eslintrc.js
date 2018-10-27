78848f5b853c32883b67dd38f914f81f047f44ec
박현우 <phu54321@naver.com>
2018-10-28 02:08:45 +0900

    Added tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,14 +20,10 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
+    '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
   }
 }
